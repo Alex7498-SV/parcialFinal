a9d2eccbd83f2ad493ca01ed58b562a06257efd3
Alex7498-SV <00068817@uca.edu.sv>
2018-12-01 15:57:45 -0600

    Finalizacion de los metodos crud

--- controller/userController.js
+++ controller/userController.js
@@ -11,6 +11,59 @@ user.create = function(req, res){
     }
     if(temp.nickname && temp.Cfav && temp.Aingreso && temp.nickname!='' && temp.Cfav!='' && temp.Aingreso!=''){
         data = new model(temp);
-        data.save()
+        data.save(function(err, saved){
+            if(err){
+                res.status(500);
+                res.json({status: 500, err})
+            } else {
+                res.json({status: 200, saved})
+            }
+        })
+    } else {
+        res.json({status: 404, message = "No ingreso datos"})
     }
 };
+
+user.getAll = function(req, res){
+    model.find({}, function(err, users){
+        if(err){
+            res.status(500);
+            res.json({status: 500, err });
+        } else {
+            res.json({status: 200, users});
+        }
+    })
+}
+
+user.get = function(req, res){
+    model.findById({_id: req.params.id}, function(err, result){
+        if(err){
+            res.status(500);
+            res.json({status: 500, err});
+        } else {
+            res.json({status: 200, result})
+        }
+    })
+}
+
+user.update = function(req, res){
+    model.findByIdAndUpdate({_id: req.params.id}, function(err, old){
+        if(err){
+            res.status(500);
+            res.json({status: 500, err});
+        } else {
+            res.json({status: 200, old})
+        }
+    })
+}
+
+user.delete = function(req, res){
+    model.findByIdAndRemove({_id: req.params.id}, function(err, old){
+        if(err){
+            res.status(500);
+            res.json({status: 500, err});
+        } else {
+            res.json({status: 200, old})
+        }
+    })
+}
