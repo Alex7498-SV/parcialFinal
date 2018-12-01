ab27bff3900a6da910e757cd050792f2685201fc
Alex7498-SV <00068817@uca.edu.sv>
2018-12-01 14:20:43 -0600

    creacion del modelo del usuario

--- model/profileModel.js
+++ model/profileModel.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
 const user = new mongoose.Schema({
-    
-})
+    nickname = {
+        type: String,
+        unique: true,
+        required: true
+   },
+   Cfav = {
+       type: String,
+       required: true
+   },
+   Aingreso = {
+       type: Date,
+       required: true
+   }
+});
+
+module.exports = mongoose.model('usuer', user);
